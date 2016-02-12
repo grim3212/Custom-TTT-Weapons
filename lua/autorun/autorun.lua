c51cf82cd2028054d853fefa115f8e157597b9d8
Grim3212 <dakotapierone@yahoo.com>
2016-02-12 13:02:57 -0800

    Branch off the Harpoon

--- lua/autorun/autorun.lua
+++ lua/autorun/autorun.lua
@@ -1,12 +1,3 @@
 --Add different things that are needed
 
-print("Grim3212 Initializing Custom Weapons")
-
-if CLIENT then
-	language.Add("AMMO_HARPOON", "Harpoon")
-end
-
-game.AddAmmoType( {
-	name = "Harpoon",
-	dmgtype = DMG_BULLET
-} )
+print("Grim3212 Initializing Guns")
